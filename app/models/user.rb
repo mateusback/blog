450c6aa397a579b9eb0746f7c860d6581a5e8b0e
Mateus Back <m.back@nextage.com.br>
2024-09-30 16:47:21 -0300

    feat: add associação entre articles e users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 ﻿class User < ApplicationRecord
+  has_many :articles
   validate :username, presence: true,
            uniqueness: { case_sensitive: false },
            length: {minimum: 3, maximum: 25}
